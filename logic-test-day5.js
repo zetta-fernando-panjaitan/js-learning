a1ec5994a370233ff4637f92594eb1a784b8f679
Fernando Michael Panjaitan <nando.panjaitan@zetta-intern.space>
2025-02-03 03:04:51 +0700

    Adding the 5th day quiz

--- logic-test-day5.js
+++ logic-test-day5.js
@@ -3,6 +3,6 @@ function containsDuplicate(nums) {
     return numSet.size !== nums.length;
 }
 
-console.log(containsDuplicate([1, 2, 3, 1])); // Output: true
-console.log(containsDuplicate([1, 2, 3, 4])); // Output: false
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // Output: true
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
